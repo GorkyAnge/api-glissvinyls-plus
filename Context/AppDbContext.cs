59c835b2f1cf33bdf43a063405b13e811f2d3c2f
GorkyAnge <gorkyange@gmail.com>
2024-10-01 00:24:41 -0500

    Add DB Context

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using glissvinyls_plus.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace glissvinyls_plus.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
