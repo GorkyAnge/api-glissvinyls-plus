6cb35e812c5b1b4f91303f125e0e138f400f55dd
GorkyAnge <gorkyange@gmail.com>
2024-10-08 19:25:45 -0500

    Create User Model

--- Models/User.cs
+++ Models/User.cs
@@ -1,12 +1,35 @@
-﻿namespace glissvinyls_plus.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace glissvinyls_plus.Models
 {
     public class User
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
         public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(100)]
         public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [StringLength(255)]
         public string Email { get; set; }
 
+        [Required]
+        [StringLength(100)]
+        public string Username { get; set; }
+
+        [Required]
+        [StringLength(100)]
         public string Password { get; set; }
+
+        [Required] // Añadir esto si es necesario
+        public string Role { get; set; } // Role for role-based access control
     }
+
 }
