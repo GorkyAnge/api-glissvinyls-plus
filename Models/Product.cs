b0de1eda4e5190010b6ad791964ffc91f07ed8c6
GorkyAnge <gorkyange@gmail.com>
2024-10-08 19:25:24 -0500

    Create Product Model

---
+++ Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace glissvinyls_plus.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+        [Required]
+        public int Stock { get; set; }
+        [Required]
+        public string Image { get; set; }
+    }
+}
