8fbc06efae19c5c663bd990c8d9fa9dbc6a408c6
GorkyAnge <gorkyange@gmail.com>
2024-10-08 19:25:11 -0500

    Add Products Table to Context

--- Context/AppDbContext.cs
+++ Context/AppDbContext.cs
@@ -11,5 +11,17 @@ namespace glissvinyls_plus.Context
 
         }
         public DbSet<User> Users { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.Property(e => e.Price)
+                      .HasColumnType("decimal(18, 2)"); // Especificar el tipo de columna
+            });
+        }
+
+
     }
 }
