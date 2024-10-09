633cb153ec12c92dd5003508dbe4e937c6ef74a8
GorkyAnge <gorkyange@gmail.com>
2024-10-08 19:26:13 -0500

    Create Simple User Model for Login

---
+++ Models/UserLogin.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace glissvinyls_plus.Models
+{
+    public class UserLogin
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+
+}
