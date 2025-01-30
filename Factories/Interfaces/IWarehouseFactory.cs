8029b93d59955a62c8e00bfb43d3b495d780aaa2
Gorky Palacios Mutis <gorky.palacios@paymentevolution.com>
2025-01-29 20:31:19 -0500

    Add IWarehouseFactory interface and WarehouseFactory class

---
+++ Factories/Interfaces/IWarehouseFactory.cs
@@ -0,0 +1,9 @@
+﻿using glissvinyls_plus.Models;
+
+namespace glissvinyls_plus.Factories.Interfaces
+{
+    public interface IWarehouseFactory
+    {
+        Warehouse CreateWarehouse(string warehouseName, string address);
+    }
+}
---
+++ Factories/WarehouseFactory.cs
@@ -0,0 +1,17 @@
+﻿using glissvinyls_plus.Factories.Interfaces;
+using glissvinyls_plus.Models;
+
+namespace glissvinyls_plus.Factories
+{
+    public class WarehouseFactory : IWarehouseFactory
+    {
+        public Warehouse CreateWarehouse(string warehouseName, string address)
+        {
+            return new Warehouse
+            {
+                WarehouseName = warehouseName,
+                Address = address
+            };
+        }
+    }
+}
